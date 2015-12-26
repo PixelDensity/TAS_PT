dcb34e3de10d92cad8f5541fad5eb5d40dd11723
PixelDensity <density.pixels@gmail.com>
2015-12-26 16:19:03 -0600

    Re Structrue

---
+++ Piano Tiles 2/Assets/Piano Tiles 2/Scripts/scale.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scale : MonoBehaviour {
+
+	Camera cam;
+
+	void Start () 
+	{
+		cam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
+	}
+	
+	void Update () 
+	{
+		transform.localScale = new Vector3(cam.orthographicSize/2 * (Screen.width/Screen.height),cam.orthographicSize/2,0f);
+	}
+}
